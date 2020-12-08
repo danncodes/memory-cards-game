df6e1376c7bca0bb23b4e4580980d12905d45d6a
Daniel Mensah <dann@daniels-mbp.home>
2020-12-08 21:02:56 +0000

    Generated HTML within JAvascript rather that statically

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-console.log('test');
+// Query Selectors
+
+let container = document.querySelector('.container');
+
+container.innerHTML =
+	'<button class="btn blue"></button><button class="btn blue"></button><button class="btn red"></button><button class="btn red"></button><button class="btn yellow"></button><button class="btn yellow"></button><button class="btn green"></button><button class="btn green"></button><button class="btn purple"></button><button class="btn purple"></button><button class="btn pink"></button><button class="btn pink"></button><button class="btn orange"></button><button class="btn orange"></button><button class="btn grey"></button><button class="btn grey"></button>';
