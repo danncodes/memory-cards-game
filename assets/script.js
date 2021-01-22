285a12e2c6f6b0460a517c77eec8a98285fdd8b3
Daniel Mensah <d.mensah@outlook.com>
2021-01-22 00:29:27 +0000

    Ensured the amount of colors is dependant on the total array length and also colors are shuffled

--- assets/script.js
+++ assets/script.js
@@ -1,152 +1,181 @@
 // Query Selectors
 
 let container = document.querySelector('.container');
-let body = document.querySelector('body');
+
+// Array of Colors
+let colors = [
+	'grey',
+	'orange',
+	'pink',
+	'purple',
+	'green',
+	'yellow',
+	'red',
+	'blue'
+];
+
+let arrayLength = 16;
+
+// Duplicate Colors
+let allColors = [
+	...colors.slice(0, arrayLength / 2),
+	...colors.slice(0, arrayLength / 2)
+];
+
+console.log(allColors);
+
+// Randomise Array
+function shuffleColors(allColors) {
+	allColors = allColors.sort((a, b) => 0.5 - Math.random());
+	console.log(allColors);
+	return allColors;
+}
+shuffleColors(allColors);
 
 container.innerHTML += `
-<div class="flip-card grey">
-	<div class="flip-card-inner grey">
-		<div class="flip-card-front grey">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back grey"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
 
-<div class="flip-card grey">
-	<div class="flip-card-inner grey">
-		<div class="flip-card-front grey">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back grey"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card orange">
-	<div class="flip-card-inner orange">
-		<div class="flip-card-front orange">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back orange"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card orange">
-	<div class="flip-card-inner orange">
-		<div class="flip-card-front orange">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back orange"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card pink">
-	<div class="flip-card-inner pink">
-		<div class="flip-card-front pink">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back pink"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card pink">
-	<div class="flip-card-inner pink">
-		<div class="flip-card-front pink">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back pink"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card purple">
-	<div class="flip-card-inner purple">
-		<div class="flip-card-front purple">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back purple"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card purple">
-	<div class="flip-card-inner purple">
-		<div class="flip-card-front purple">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back purple"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card green">
-	<div class="flip-card-inner green">
-		<div class="flip-card-front green">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back green"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card green">
-	<div class="flip-card-inner green">
-		<div class="flip-card-front green">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back green"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card yellow">
-	<div class="flip-card-inner yellow">
-		<div class="flip-card-front yellow">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back yellow"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card yellow">
-	<div class="flip-card-inner yellow">
-		<div class="flip-card-front yellow">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back yellow"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card red">
-	<div class="flip-card-inner red">
-		<div class="flip-card-front red">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back red"></div>
+		<div class="flip-card-bac"></div>
 	</div>
 </div>
-<div class="flip-card red">
-	<div class="flip-card-inner red">
-		<div class="flip-card-front red">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back red"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
-<div class="flip-card blue">
-	<div class="flip-card-inner blue">
-		<div class="flip-card-front blue">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back blue"></div>
+		<div class="flip-card-back "></div>
 	</div>
 </div>
-<div class="flip-card blue">
-	<div class="flip-card-inner blue">
-		<div class="flip-card-front blue">
+<div class="flip-card">
+	<div class="flip-card-inner">
+		<div class="flip-card-front">
 			<h2>Daniel Mensah</h2>
 			<i class="fab fa-linkedin"></i>
 		</div>
-		<div class="flip-card-back blue"></div>
+		<div class="flip-card-back"></div>
 	</div>
 </div>
 `;
@@ -158,10 +187,13 @@ function selectCard(e) {
 	let selectedCard = e.target;
 
 	// Flip Card
-	if (selectedCard.className.includes('card')) {
+	if (
+		selectedCard.className.includes('card') &&
+		selectedCard.parentElement.classList != 'container'
+	) {
 		selectedCard.parentElement.classList.toggle('flip');
 	}
 }
 
 // Event Listeners
-body.addEventListener('click', selectCard);
+container.addEventListener('click', selectCard);
